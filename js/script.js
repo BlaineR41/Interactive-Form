bec628d97da31df540e9f68a65407fd6b9e18e7f
BlaineR41 <blaineruttan@gmail.com>
2022-03-12 12:31:31 -0500

    Step 2

--- js/script.js
+++ js/script.js
@@ -10,5 +10,19 @@ const form = document.querySelector('form');
 const userName = document.getElementById('name');
 const email = document.getElementById('email');
 
+/*Job Role Section*/
+const selectJob = document.getElementById('title');
+const otherJobRole = document.getElementById('other-job-role');
+
 //Set focus state on Name on initial load
 userName.focus();
+
+//Hides "other Job Role" field by default
+otherJobRole.style.display = 'none';
+
+//Displays or Hides the "other job role" option depending on the 'Job role' selected
+selectJob.addEventListener('change', e => {
+  e.target.value === 'other'
+      ? otherJobRole.style.display = 'block'
+      : otherJobRole.style.diplay = 'none';
+});
