872729ed17e995ec51e4e548e5abe036a8db0e46
BlaineR41 <blaineruttan@gmail.com>
2022-03-12 13:03:32 -0500

    Step 4

--- js/script.js
+++ js/script.js
@@ -2,8 +2,9 @@
 Treehouse FSJS Techdegree:
 * Project 3 - Interactive Form
 */
-
+/********************************/
 /* Var Bank to store form inputs*/
+/*******************************/
 
 /*Basic Info Section*/
 const form = document.querySelector('form');
@@ -14,8 +15,23 @@ const email = document.getElementById('email');
 const selectJob = document.getElementById('title');
 const otherJobRole = document.getElementById('other-job-role');
 
+/*T-Shirt Info Section*/
+const design = document.getElementById('design');
+const color = document.getElementById('color');
+const colorOption = color.children;
+
+/*Register For Activities Section*/
+const activites = document.getElementById('activites')
+/********************/
+/*Basic Info Section*/
+/********************/
+
 //Set focus state on Name on initial load
 userName.focus();
+
+/******************/
+/*Job Role Section*/
+/******************/
 
 //Hides "other Job Role" field by default
 otherJobRole.style.display = 'none';
@@ -26,3 +42,30 @@ selectJob.addEventListener('change', e => {
       ? otherJobRole.style.display = 'block'
       : otherJobRole.style.diplay = 'none';
 });
+
+/***********************/
+/*T-Shirt info Section*/
+/*********************/
+
+//Disables the color field by default
+color.disabled = true;
+
+//Displays the color option after selecting a design then displaysdifferent color options based on the design selected
+design.addEventListener('change', e => {
+    color.disabled = false;
+    for ( let i = 0; i < colorOption.length; i++ ) {
+        const designSelected = colorOption[i].getAttribute('data-theme');
+        const target = e.target.value;
+        if (designSelected === target ) {
+          colorOption[i].hidden = false;
+          colorOption[i].setAttribute('selected', 'selected');
+        } else {
+            colorOption[i].hidden = true;
+            colorOption[i].removeAttribute('selected');
+        }
+    }
+});
+
+/**********************************/
+/*Register For Activities Section*/
+/********************************/
